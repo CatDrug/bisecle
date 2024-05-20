bba5450b6a7b669bb4d9391c4643bc4213a755da
DESKTOP-88CFRPE\GoodWin <CatDrug>
2024-05-20 18:38:54 +0400

    upd: links

--- ELK/install.sh
+++ ELK/install.sh
@@ -10,15 +10,15 @@ sysctl -p
 echo "TZ=Europe/Moscow" >> /opt/.global
 mkdir /opt/elk
 cd /opt/elk
-wget https://docker.yml
+wget https://github.com/CatDrug/bisecle/blob/main/ELK/docker-compose.yml
 docker-compose up -d
 docker-compose stop
 cd /var/lib/docker/volumes/elk_config/_data
 rm elasticsearch.yml
-wget https://elastic.yml
+wget https://github.com/CatDrug/bisecle/blob/main/ELK/elasticsearch.yml
 cd /var/lib/docker/volumes/elk_kconfig/_data
 rm kibana.yml
-wget https://kibana.yml
+wget https://github.com/CatDrug/bisecle/blob/main/ELK/kibana.yml
 cd /opt/elk
 docker-compose up -d
 docker exec elastic bash bin/elasticsearch-users useradd goodwin -p P@ssw0rd -r superuser
