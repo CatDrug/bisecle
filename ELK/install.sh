f45ceda00064d4100cb56958e9aaa8361e52fa59
DESKTOP-88CFRPE\GoodWin <CatDrug>
2024-05-20 18:35:48 +0400

    add: ELK

---
+++ ELK/install.sh
@@ -0,0 +1,24 @@
+apt update
+apt install docker docker.io -y
+apt install docker-compose docker.io -y
+systemctl start docker
+systemctl enable docker
+echo "vm.max_map_count=262144" >> /etc/sysctl.conf
+echo "vm.overcommit_memory = 1" >> /etc/sysctl.conf
+sysctl -p
+
+echo "TZ=Europe/Moscow" >> /opt/.global
+mkdir /opt/elk
+cd /opt/elk
+wget https://docker.yml
+docker-compose up -d
+docker-compose stop
+cd /var/lib/docker/volumes/elk_config/_data
+rm elasticsearch.yml
+wget https://elastic.yml
+cd /var/lib/docker/volumes/elk_kconfig/_data
+rm kibana.yml
+wget https://kibana.yml
+cd /opt/elk
+docker-compose up -d
+docker exec elastic bash bin/elasticsearch-users useradd goodwin -p P@ssw0rd -r superuser
